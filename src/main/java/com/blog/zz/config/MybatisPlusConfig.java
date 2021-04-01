c749d32969e4f327a966a07360c4947880233c5f
zzoxi <961296023@qq.com>
2021-04-01 21:38:36 +0800

    乱七八糟弄了一下

---
+++ src/main/java/com/blog/zz/config/MybatisPlusConfig.java
@@ -0,0 +1,9 @@
+package com.blog.zz.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan()
+public class MybatisPlusConfig {
+}
